867f60f48b64ed378cf8d7a55511792286e92664
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-05-27 01:35:22 +0530

    Used reducer to manipulate state in Functional component

--- book-list/src/contexts/BookContext.js
+++ book-list/src/contexts/BookContext.js
@@ -1,25 +1,14 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useReducer } from 'react'
+import { bookReducer } from '../reducers/BookReducer'
 // import uuid from 'uuid/v1'
 
 export const BookContext = createContext()
 
 const BookContextProvider = (props) => {
-  const [books, setBooks] = useState([
-    { title: 'Wings of Fire', author: 'APJ Abdul Kalam', id: 1 },
-    { title: 'Eloquent Javascript', author: 'Marjin Haverbeke', id: 2 },
-    { title: 'Two States', author: 'Chetan Bhagat', id: 3 }
-  ])
-
-  const addBook = (title, author) => {
-    setBooks([...books, { title, author, id: Math.random() }])
-  }
-
-  const deleteBook = (id) => {
-    setBooks(books.filter(book => book.id !== id))
-  }
+  const [books, dispatch] = useReducer(bookReducer, [])
 
   return (
-    <BookContext.Provider value={{ books, addBook, deleteBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
     </BookContext.Provider>
   )
