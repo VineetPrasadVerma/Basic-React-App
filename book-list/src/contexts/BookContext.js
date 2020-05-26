aa3a5da9b74d56a8bf266ad45706697aa2f0db4a
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-05-27 01:53:01 +0530

    Add local storage in app to hold data

--- book-list/src/contexts/BookContext.js
+++ book-list/src/contexts/BookContext.js
@@ -1,11 +1,18 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useReducer, useEffect } from 'react'
 import { bookReducer } from '../reducers/BookReducer'
 // import uuid from 'uuid/v1'
 
 export const BookContext = createContext()
 
 const BookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(bookReducer, [])
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localBooks = window.localStorage.getItem('books')
+    return localBooks ? JSON.parse(localBooks) : []
+  })
+
+  useEffect(() => {
+    window.localStorage.setItem('books', JSON.stringify(books))
+  }, [books])
 
   return (
     <BookContext.Provider value={{ books, dispatch }}>
